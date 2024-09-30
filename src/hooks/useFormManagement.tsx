dbb190bfb828ce7c3c22e4cc8c6daac2ddf17f0c
Vioancer <justindanner96@gmail.com>
2024-09-29 17:15:54 -0700

    feat: add custom hook of form and history

---
+++ src/hooks/useFormManagement.tsx
@@ -0,0 +1,77 @@
+import { useState, useCallback } from "react";
+import { z } from "zod";
+import { imageGenerationSchema } from "../validation/imageGenerationSchema";
+import { ImageGenerationOptions } from "../hooks/useImageGeneration";
+
+export const useFormManagement = (
+  generateImage: (options: ImageGenerationOptions) => void
+) => {
+  const [prompt, setPrompt] = useState("");
+  const [negative_prompt, setNegativePrompt] = useState("");
+  const [advancedOptions, setAdvancedOptions] = useState(false);
+  const [width, setWidth] = useState(768);
+  const [height, setHeight] = useState(768);
+  const [num_outputs, setNumOutputs] = useState(1);
+  const [image, setImageUrl] = useState("");
+  const [mask, setMaskUrl] = useState("");
+  const [prompt_strength, setPromptStrength] = useState(0.8);
+  const [errorMessages, setErrorMessages] = useState<string[] | null>([]);
+
+  const handleSubmit = useCallback(() => {
+    const formData = {
+      prompt,
+      negative_prompt,
+      width,
+      height,
+      num_outputs,
+      image,
+      mask,
+      prompt_strength,
+    };
+
+    const validationResult = imageGenerationSchema.safeParse(formData);
+
+    if (validationResult.success) {
+      const imageGenerationOptions: ImageGenerationOptions =
+        validationResult.data;
+      generateImage(imageGenerationOptions);
+      setErrorMessages(null);
+    } else {
+      setErrorMessages(
+        validationResult.error.errors.map((error) => error.message)
+      );
+    }
+  }, [
+    prompt,
+    negative_prompt,
+    width,
+    height,
+    num_outputs,
+    image,
+    mask,
+    prompt_strength,
+  ]);
+
+  return {
+    prompt,
+    setPrompt,
+    negative_prompt,
+    setNegativePrompt,
+    advancedOptions,
+    setAdvancedOptions,
+    width,
+    setWidth,
+    height,
+    setHeight,
+    num_outputs,
+    setNumOutputs,
+    image,
+    setImageUrl,
+    mask,
+    setMaskUrl,
+    prompt_strength,
+    setPromptStrength,
+    errorMessages,
+    handleSubmit,
+  };
+};
---
+++ src/hooks/useHistory.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+interface HistoryEntry {
+  prompt: string;
+  output: string[] | null;
+}
+
+export const useHistory = (output: string[] | null, prompt: string) => {
+  const [history, setHistory] = useState<HistoryEntry[]>([]);
+
+  useEffect(() => {
+    if (output?.length) {
+      const newEntry: HistoryEntry = { prompt, output };
+      setHistory((prevHistory) => [newEntry, ...prevHistory]);
+    } else {
+      setHistory([]);
+    }
+  }, [output, prompt]);
+
+  return history;
+};
